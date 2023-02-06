7fd4832cb3e160f76ee816aa75c11722b02a6077
Thys Vanderschoot <thysvanderschoot@gmail.com>
2023-02-05 22:37:23 -0500

    Initial Commit, Artifact appears in game

---
+++ PartialEclipse8/PartialEclipse8.cs
@@ -0,0 +1,48 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using RoR2;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Security.Permissions;
+using R2API;
+using UnityEngine;
+
+[assembly: AssemblyVersion(PartialEclipse8.PartialEclipse8Plugin.Version)]
+namespace PartialEclipse8
+{
+    [BepInPlugin(GUID, Name, Version)]
+    [BepInDependency("com.bepis.r2api")]
+    [R2API.Utils.R2APISubmoduleDependency(nameof(LanguageAPI), nameof(RecalculateStatsAPI), nameof(ItemAPI), nameof(EliteAPI), nameof(ContentAddition))]
+
+    public class PartialEclipse8Plugin : BaseUnityPlugin
+    {
+        public const string GUID = "com.RegalTurtle.PartialEclipse";
+        public const string Name = "Partial Eclipse";
+        public const string Version = "0.0.0";
+
+        private static readonly HashSet<NetworkUser> votedForEclipse = new HashSet<NetworkUser>();
+
+        private static void PreGameControllerStartRun(Action<PreGameController> orig, PreGameController self)
+        {
+            votedForEclipse.Clear();
+            var choice = RuleCatalog.FindChoiceDef("Artifacts.PartialEclipse8.On");
+            foreach (var user in NetworkUser.readOnlyInstancesList)
+            {
+                var voteController = PreGameRuleVoteController.FindForUser(user);
+                var isMetamorphosisVoted = voteController.IsChoiceVoted(choice);
+
+                if (isMetamorphosisVoted)
+                {
+                    votedForEclipse.Add(user);
+                }
+            }
+            orig(self);
+        }
+
+        public void Awake()
+        {
+            new PartialEclipse8Artifact();
+        }
+    }
+}
---
+++ PartialEclipse8/PartialEclipse8Artifact.cs
@@ -0,0 +1,88 @@
+﻿using R2API;
+using RoR2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.Serialization;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+public class PartialEclipse8Artifact
+{
+    public static ArtifactDef artifact;
+
+    public PartialEclipse8Artifact()
+    {
+        LanguageAPI.Add("PARTIALECLIPSE_PARTIALECLIPSE8_NAME", "Artifact of Partial Eclipse");
+        LanguageAPI.Add("PARTIALECLIPSE_PARTIALECLIPSE8_DESC", "Applies Eclipse 8 for people who select the artifact.");
+
+        artifact = ScriptableObject.CreateInstance<ArtifactDef>();
+        artifact.cachedName = "PartialEclipse8";
+        artifact.nameToken = "PARTIALECLIPSE_PARTIALECLIPSE8_NAME";
+        artifact.descriptionToken = "PARTIALECLIPSE_PARTIALECLIPSE8_DESC";
+        artifact.smallIconSelectedSprite = CreateSprite(null, Color.magenta);
+        artifact.smallIconDeselectedSprite = CreateSprite(null, Color.gray);
+        //artifact.smallIconDeselectedSprite = RiskyArtifactsPlugin.assetBundle.LoadAsset<Sprite>("texArrogDisabled.png");
+        //artifact.smallIconSelectedSprite = RiskyArtifactsPlugin.assetBundle.LoadAsset<Sprite>("texArrogEnabled.png");
+        //RiskyArtifactsPlugin.FixScriptableObjectName(artifact);
+        ContentAddition.AddArtifactDef(artifact);
+    }
+
+    public static Sprite CreateSprite(byte[] resourceBytes, Color fallbackColor)
+    {
+        // Create a temporary texture, then load the texture onto it.
+        var tex = new Texture2D(32, 32, TextureFormat.RGBA32, false);
+        try
+        {
+            if (resourceBytes == null)
+            {
+                FillTexture(tex, fallbackColor);
+            }
+            else
+            {
+                tex.LoadImage(resourceBytes, false);
+                tex.Apply();
+                CleanAlpha(tex);
+            }
+        }
+        catch (Exception e)
+        {
+            FillTexture(tex, fallbackColor);
+        }
+
+        return Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), new Vector2(31, 31));
+    }
+
+    private static Texture2D FillTexture(Texture2D tex, Color color)
+    {
+        var pixels = tex.GetPixels();
+        for (var i = 0; i < pixels.Length; ++i)
+        {
+            pixels[i] = color;
+        }
+
+        tex.SetPixels(pixels);
+        tex.Apply();
+
+        return tex;
+    }
+
+    private static Texture2D CleanAlpha(Texture2D tex)
+    {
+        var pixels = tex.GetPixels();
+        for (var i = 0; i < pixels.Length; ++i)
+        {
+            if (pixels[i].a < 0.05f)
+            {
+                pixels[i] = Color.clear;
+            }
+        }
+
+        tex.SetPixels(pixels);
+        tex.Apply();
+
+        return tex;
+    }
+}
